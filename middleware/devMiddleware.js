805008ff728a4eee1c70fbfd777142c1edcf323b
Roman Shtylman <shtylman@gmail.com>
2016-09-28 12:04:18 -0700

    fix devMiddleware to properly use await
    prevents 404 when loading the bundle before the bundle has been built

--- middleware/devMiddleware.js
+++ middleware/devMiddleware.js
@@ -3,11 +3,23 @@ import devMiddleware from 'webpack-dev-middleware'
 export default (compiler, opts) => {
   const expressMiddleware = devMiddleware(compiler, opts)
   return async (ctx, next) => { // eslint-disable-line
-    await expressMiddleware(ctx.req, {
-      end: (content) => {
-        ctx.body = content
-      },
-      setHeader: ctx.set.bind(ctx)
-    }, next)
+    await new Promise((resolve, reject) => {
+      const nextShim = function(err) {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        resolve(next())
+      }
+
+      expressMiddleware(ctx.req, {
+        end: (content) => {
+          ctx.body = content
+          resolve();
+        },
+        setHeader: ctx.set.bind(ctx)
+      }, nextShim)
+    });
   }
 }
